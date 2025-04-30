9107796c683471190595c1af36574c38a8c850bc
Xtron135 <xtron135@gmail.com>
2025-04-30 18:13:01 +0800

    added gui test

---
+++ Additional-Contents/guitest.py
@@ -0,0 +1,37 @@
+from guizero import App, Box, Text, PushButton, Picture
+
+# Widget and Layout -----
+mainWin = App(title="Test App", height=720, width=1280)
+
+leftSec = Box(mainWin, height=720, width=500, align="left")
+rightSec = Box(mainWin, height=720, width=780, align="right")
+leftSec.bg = "blue"
+rightSec.bg = "green"
+
+## Left Side Layout
+left1 = Box(leftSec, height=40, width=300)
+left1.bg = "grey"
+listtitle = Box(leftSec, height=45, width=300)
+listtitle.bg = "yellow"
+dmlist = Text(listtitle, text="Scanned Data Matrix", align="bottom", size=17)
+
+listbox = Box(leftSec, height=500, width=300)
+listbox.bg = "red"
+
+## Right Side
+
+right1 = Box(rightSec, height=40, width=600)
+right1.bg = "grey"
+imgbox = Box(rightSec, height=400, width=600)
+imgbox.bg = "yellow"
+altimg = Text(imgbox, text="PICTURE\nHERE", size=90)
+
+spacer = Box(rightSec, height=280, width=99, align="left")
+spacer.bg = "red"
+bt1 = PushButton(rightSec, height=5, width=15, align="left")
+bt2 = PushButton(rightSec, height=5, width=15, align="left")
+bt3 = PushButton(rightSec, height=5, width=15, align="left")
+bt4 = PushButton(rightSec, height=5, width=15, align="left")
+
+# App Execute -----
+mainWin.display()
